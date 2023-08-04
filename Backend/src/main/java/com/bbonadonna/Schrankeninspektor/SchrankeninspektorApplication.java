ab0dae8ad1764b0a42637c3cd2a72fe5d89bac22
BrunoCookie <brunomail37@gmail.com>
2023-08-04 15:40:56 +0200

    Init Commit

---
+++ Backend/src/main/java/com/bbonadonna/Schrankeninspektor/SchrankeninspektorApplication.java
@@ -0,0 +1,13 @@
+package com.bbonadonna.Schrankeninspektor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchrankeninspektorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchrankeninspektorApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/bbonadonna/Schrankeninspektor/SchrankeninspektorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbonadonna.Schrankeninspektor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchrankeninspektorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
